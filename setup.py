cfac7e30c3142585d85fa2723852888f59d86aa5
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-14 09:23:31 -0500

    Setup files and requirements

---
+++ setup.py
@@ -0,0 +1,42 @@
+# This file is part of pyRDDLGym.
+
+# pyRDDLGym is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published by
+# the Free Software Foundation.
+
+# pyRDDLGym is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with pyRDDLGym. If not, see <https://opensource.org/licenses/MIT>.
+
+from setuptools import setup, find_packages
+
+
+setup(
+      name='rddl-ide',
+      version='0.1',
+      author="Michael Gimelfarb",
+      author_email="mike.gimelfarb@mail.utoronto.ca",
+      description="IDE for pyRDDLGym",
+      license="MIT License",
+      url="https://github.com/pyrddlgym-project/rddl-ide",
+      packages=find_packages(),
+      install_requires=[
+          'pyRDDLGym>=2.0'
+        ],
+      python_requires=">=3.8",
+      package_data={'': ['*.cfg', '*.rddl']},
+      include_package_data=True,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
