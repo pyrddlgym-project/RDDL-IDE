296cd427af33b213b6544443b47debe71850c100
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-14 22:16:45 -0500

    Prevent rddlrepo domain content overwriting

--- rddl_ide/core/menubar.py
+++ rddl_ide/core/menubar.py
@@ -94,6 +94,9 @@ def assign_menubar_functions(domain_window, inst_window, policy_window,
             _window_from_file(inst_window, inst_editor, 'Instance', inst_file) 
               
             close_me()
+            
+            # do not override automatically
+            domain_file, inst_file = None, None
         
         tk.Button(master, text='Load', command=select_problem).grid(
             row=3, column=0, sticky=tk.W, pady=4)
